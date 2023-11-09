f777fecb6f1ff14e9ee9295259d8d769759fa6cf
PurpleMyst <PurpleMyst@users.noreply.github.com>
2023-11-09 16:31:02 +0100

    feat: CLI arguments for voice cloning + split inputs/outputs into directories

--- frog_tts_petscop.py
+++ frog_tts_petscop.py
@@ -16,25 +16,31 @@ import numpy as np
 import scipy
 import srt
 from joblib import Memory
-from rich import print
+from rich.console import Console
 from TTS.api import TTS
 
-MEMORY = Memory("joblib_cache", verbose=0, mmap_mode="r+")
+print = Console(highlight=False).print
+
+MEMORY = Memory("joblib_cache", verbose=0)
 
 BATCH_SIZE = 500
 SILENCE_DURATION = 0.1
 EMPTY_STRING_DURATION = 2 * SILENCE_DURATION
 
+VIDEO_ENCODER = "h264_nvenc"  # h264_nvenc is faster than libx264 but less configurabile
+AUDIO_ENCODER = "aac"  # libopus is faster & better than aac but less compatible
+
 
 @MEMORY.cache(ignore=["tts"])
-def generate_sentence(tts, sentence: str):
+def generate_sentence(tts, sentence: str, *, language: str | None, speaker: str | None):
     start = perf_counter()
     with redirect_stdout(StringIO()):
-        audio_array = tts.tts(sentence)
+        audio_array = tts.tts(sentence, language=language, speaker_wav=speaker)
     gen_time = perf_counter() - start
     return audio_array, gen_time
 
 
+# taken from the itertools 3.12 docs
 def batched(iterable, n):
     from itertools import islice
 
@@ -46,51 +52,57 @@ def batched(iterable, n):
         yield batch
 
 
-def convert_batch(tts, image, sentences, output):
+def convert_batch(tts, batch, *, output, image, language, speaker):
     sample_rate = tts.synthesizer.output_sample_rate
 
     audio_arrays = []
     subtitles = []
 
     timestamp = timedelta(seconds=0)
+    ki = None
 
-    padding = len(str(len(sentences)))
-    for i, sentence in enumerate(sentences, start=1):
-        h, ms = divmod(timestamp.total_seconds(), 60 * 60)
-        m, s = divmod(ms, 60)
-        print(
-            rf"[yellow]\[{i:{padding}}/{len(sentences)} - {h:02.0f}:{m:02.0f}:{s:02.0f}]"
-            rf" {sentence!r}"
-        )
-
-        if not sentence.strip():
-            silence = np.zeros(int(EMPTY_STRING_DURATION * sample_rate))
-            audio_arrays.append(silence)
-            timestamp += timedelta(seconds=EMPTY_STRING_DURATION)
-            continue
-
-        sentence_audio, gen_time = generate_sentence(tts, sentence)
-        duration = timedelta(seconds=len(sentence_audio) / sample_rate)
-        ratio = gen_time / duration.total_seconds()
-        print(
-            f"\tGenerated [red]{duration.total_seconds():.2f}s[/] of audio in"
-            f" [red]{gen_time:.2f}s[/] ([bold cyan]{ratio:.2f}x realtime[/])"
-        )
-
-        audio_arrays.append(sentence_audio)
-        silence = np.zeros(int(SILENCE_DURATION * sample_rate))
-        audio_arrays.append(silence)
-
-        subtitles.append(
-            srt.Subtitle(
-                index=i,
-                start=timestamp,
-                end=timestamp + duration,
-                content=srt.make_legal_content(sentence),
+    try:
+        padding = len(str(len(batch)))
+        for i, text in enumerate(batch, start=1):
+            h, ms = divmod(timestamp.total_seconds(), 60 * 60)
+            m, s = divmod(ms, 60)
+            print(
+                rf"[yellow]\[{i:{padding}}/{len(batch)} - {h:02.0f}:{m:02.0f}:{s:02.0f}]"
+                rf" {text!r}"
             )
-        )
 
-        timestamp += duration + timedelta(seconds=SILENCE_DURATION)
+            if not text.strip():
+                silence = np.zeros(int(EMPTY_STRING_DURATION * sample_rate))
+                audio_arrays.append(silence)
+                timestamp += timedelta(seconds=EMPTY_STRING_DURATION)
+                continue
+
+            sentence_audio, gen_time = generate_sentence(
+                tts, text, language=language, speaker=speaker
+            )
+            duration = timedelta(seconds=len(sentence_audio) / sample_rate)
+            ratio = gen_time / duration.total_seconds()
+            print(
+                f"\tGenerated [red]{duration.total_seconds():.2f}s[/] of audio in"
+                f" [red]{gen_time:.2f}s[/] ([bold cyan]{ratio:.2f}x realtime[/])"
+            )
+
+            audio_arrays.append(sentence_audio)
+            silence = np.zeros(int(SILENCE_DURATION * sample_rate))
+            audio_arrays.append(silence)
+
+            subtitles.append(
+                srt.Subtitle(
+                    index=i,
+                    start=timestamp,
+                    end=timestamp + duration,
+                    content=srt.make_legal_content(text),
+                )
+            )
+
+            timestamp += duration + timedelta(seconds=SILENCE_DURATION)
+    except KeyboardInterrupt as e:
+        ki = e
 
     print(f"Writing output to [bold cyan]{output}[/]")
 
@@ -100,90 +112,101 @@ def convert_batch(tts, image, sentences, output):
         Path("petscop.srt").write_text(srt.compose(subtitles), encoding="utf-8")
 
         # Use ffmpeg to combine the WAV and SRT files into a video file, along with a pretty picture.
-        filter_complex = ";".join(
-            (
-                "[0:v]pad=ceil(iw/2)*2:ceil(ih/2)*2[img]",
-                "[1:a]showwaves=mode=line:s=xga:colors=Blue@0.5|Yellow@0.5:scale=sqrt,format=yuva420p[waves]",
-                "[img][waves]overlay=x=(W-w)/2:y=(H-h)/2[out]",
-            )
+        # ---
+        # Pad the image to the nearest even width/height, otherwise we get an error in libx264.
+        img_filter = "[0:v]pad=ceil(iw/2)*2:ceil(ih/2)*2[img]"
+        waves_filter = (
+            "[1:a]showwaves=mode=line:s=xga:colors=Blue@0.5:scale=sqrt,format=yuva420p[waves]"
         )
+        overlay_filter = "[img][waves]overlay=x=(W-w)/2:y=(H-h)/2[out]"
+        filter_complex = f"{img_filter};{waves_filter};{overlay_filter}"
+        # fmt: off
         cmd = (
             "ffmpeg",
             "-hide_banner",
-            "-loop",
-            "1",
+            "-loop", "1",
             "-y",
-            "-i",
-            image,
-            "-i",
-            "output.wav",
-            "-i",
-            "petscop.srt",
-            # Pad the image to the nearest even width/height, othertwise we get an error.
-            "-filter_complex",
-            filter_complex,
-            "-map",
-            "[out]",
-            "-map",
-            "1:a",
-            "-map",
-            "2:s",
-            "-c:v",
-            "libx264",
-            "-crf",
-            "28",
+            "-i", image,
+            "-i", "output.wav",
+            "-i", "petscop.srt",
+            "-filter_complex", filter_complex,
+            "-map", "[out]",
+            "-map", "1:a",
+            "-map", "2:s",
+            "-c:v", VIDEO_ENCODER,
+            "-crf", "28",
             "-shortest",
-            "-c:s",
-            "copy",
+            "-c:s", "mov_text",
+            "-c:a", AUDIO_ENCODER,
             output,
         )
         run(cmd, check=True)
+        # fmt: on
     finally:
         Path("output.wav").unlink(missing_ok=True)
         Path("petscop.srt").unlink(missing_ok=True)
+
+    if ki is not None:
+        raise ki
 
     return output
 
 
 def concatenate(files, output):
     files_file = Path(output).with_suffix(".files.txt")
-    files_file.write_text("\n".join(f"file '{f}'" for f in files))
+    files_file.write_text("\n".join(f"file '{Path(f).resolve()}'" for f in files))
     try:
+        # fmt: off
         cmd = (
             "ffmpeg",
             "-hide_banner",
             "-y",
-            "-f",
-            "concat",
-            "-safe",
-            "0",
-            "-i",
-            files_file,
-            "-c",
-            "copy",
+            "-f", "concat",
+            "-safe", "0",
+            "-i", files_file,
+            "-c", "copy",
+            "-c:s", "copy",
             output,
         )
         run(cmd, check=True)
+        # fmt: on
     finally:
         files_file.unlink(missing_ok=True)
 
 
-def main(input_filepath: str) -> None:
+def break_sentences(text: str) -> list[str]:
+    return list(chain.from_iterable(sent.split("\n") for sent in nltk.sent_tokenize(text)))
+
+
+@argh.arg("input_filepath", type=str, help="The plain text file to convert to speech.")
+@argh.arg(
+    "-l",
+    "--language",
+    type=str,
+    help="The language to use for the text-to-speech. Defaults to None as the default model is not multi-language.",
+)
+@argh.arg(
+    "-s",
+    "--speaker",
+    type=str,
+    help="A WAV file to use as the speaker's voice. If left unspecified, Jenny will be used (she's faster than cloning a speaker)",
+)
+def main(input_filepath: str, language: str | None = None, speaker: str | None = None) -> None:
     nltk.download("punkt")
 
-    tts = TTS("tts_models/en/jenny/jenny").to("cuda")
+    if speaker is None:
+        tts = TTS("tts_models/en/jenny/jenny")
+    else:
+        tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2")
+    tts = tts.to("cuda")
+
+    Path("output").mkdir(exist_ok=True)
 
     # Split the text into sentences, and then split each sentence into lines to ensure that the
     # subtitles don't get too long.
-    all_sentences = list(
-        chain.from_iterable(
-            sent.split("\n")
-            for sent in nltk.sent_tokenize(Path(input_filepath).read_text(encoding="utf-8"))
-        )
-    )
-
-    prefix = f"petscop-{uuid4()}"
-    image = choice(list(Path().glob("*.jpg")))
+    all_sentences = break_sentences(Path(input_filepath).read_text(encoding="utf-8"))
+    prefix = f"output/{Path(input_filepath).stem}-{uuid4()}"
+    image = choice(list(Path("images").glob("*")))
     files = []
 
     print("\033c", end="")  # Clear console
@@ -192,23 +215,28 @@ def main(input_filepath: str) -> None:
     # This is done to avoid WAV limitations (and also because otherwise we'd use a lot of RAM)
     # We can just concatenate it all together later.
     try:
-        for j, sentences in enumerate(batched(all_sentences, BATCH_SIZE), start=1):
-            print(
-                "[bold magenta]Generating audio for batch"
-                f" {j}/{ceil(len(all_sentences) / BATCH_SIZE)}"
-            )
-            print(f"[magenta]{'-' * get_terminal_size().columns}[/]")
-            output = f"{prefix}-{j:02}.mkv"
-            files.append(output)  # so it gets deleted if something goes wrong
-            convert_batch(tts, image, sentences, output)
+        try:
+            for j, sentences in enumerate(batched(all_sentences, BATCH_SIZE), start=1):
+                print(
+                    "[bold magenta]Generating audio for batch"
+                    f" {j}/{ceil(len(all_sentences) / BATCH_SIZE)}"
+                )
+                print(f"[magenta]{'-' * get_terminal_size().columns}[/]")
+                output = f"{prefix}-{j:02}.mp4"
+                files.append(output)  # so it gets deleted if something goes wrong
+                convert_batch(
+                    tts, sentences, output=output, image=image, language=language, speaker=speaker
+                )
+        except KeyboardInterrupt:
+            pass
 
-        print(f"Concatenating all videos into [bold cyan]{prefix}.mkv[/]")
-        concatenate(files, f"{prefix}.mkv")
+        print(f"Concatenating all videos into [bold cyan]{prefix}.mp4[/]")
+        concatenate(files, f"{prefix}.mp4")
     finally:
         for file in files:
             Path(file).unlink(missing_ok=True)
 
-    print(f"Done! :frog:")
+    print("Done! :frog:")
 
 
 if __name__ == "__main__":
--- justfile
+++ justfile
@@ -1,15 +1,12 @@
 set windows-shell := ["pwsh", "-NoLogo", "-NoProfileLoadTime", "-Command"]
 set dotenv-load
 
-run-frog:
-    cls && venv/Scripts/python.exe frog_tts_petscop.py
+run +args:
+    venv/Scripts/python.exe frog_tts_petscop.py {{args}}
 
 format:
-    black . -l100 -t py311 --preview
+    ruff format .
     isort .
 
 install arg:
     venv/Scripts/python.exe -m pip install "{{arg}}"
-
-#run-bark:
-#    cls && venv/Scripts/python.exe bark_petscop.py
